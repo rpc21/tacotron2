1757f3b4ad0a37411b08d8ca9505abfdd1ea74ef
Ryan Culhane <rpc21@research-tarokhlab-03.oit.duke.edu>
2019-07-17 11:44:09 -0400

    added code to evaluate the latent model

--- latent_evaluation.py
+++ latent_evaluation.py
@@ -1,16 +1,21 @@
 import pdb
-
+import pickle
 import torch
-
+import argparse
 import hparams
 from model import GMVAE
-from train import prepare_dataloaders, load_checkpoint
+from classification import prepare_dataloaders, load_checkpoint
+from hparams import create_hparams
+from torch.utils.data import DataLoader
+import numpy as np
 
 
 def load_latent_model(hparams, path_to_checkpoint):
-    checkpoint = torch.load(hparams, path_to_checkpoint)
-    model = GMVAE(hparams)
+    checkpoint = torch.load(path_to_checkpoint)
+#    pdb.set_trace()
+    model = GMVAE(hparams, True)
     model.load_state_dict(checkpoint['state_dict'])
+#    model.supervised = True
     return model
 
 
@@ -39,18 +44,59 @@ def evaluate_latent_model(checkpoint_path):
     print('===========================Number of parameters===================================')
     print(str(sum(p.numel() for p in model.parameters() if p.requires_grad)))
     is_overflow = False
+
     for i, batch in enumerate(train_loader):
         for param_group in optimizer.param_groups:
             param_group['lr'] = learning_rate
 
-        model.zero_grad()
-        x, y = model.parse_batch(batch)
-        recon, mu, logvar, x_after_mean = model(x)
-        pdb.set_trace()
+        with torch.no_grad():
+#        pdb.set_trace()
+            model.supervised = True
+            x, y = model.parse_batch(batch)
+            recon, mu, logvar, x_after_mean = model(x)
+#            pdb.set_trace()
+            w = torch.squeeze(x_after_mean).view(-1).cpu().numpy()
+            v = torch.squeeze(recon).view(-1).cpu().numpy()
+            assert(type(w) == type(np.array([])))
+            print(w.shape, v.shape, type(w), type(v))
+            inputs.append(w)
+            outputs.append(v)
+            print(len(inputs))
+#            outputs = torch.cat((outputs, torch.squeeze(recon)), dim=0)
+#        pdb.set_trace()
 
-        print('Iteration {} is complete')
-        iteration += 1
+            print('Iteration {} is complete'.format(iteration))
+            iteration += 1
+    print(len(outputs))
+    print(len(inputs))
+    print(inputs[1])
+    with open('/scratch/speech/inputs.pkl','wb+') as f:
+        pickle.dump(inputs, f)
+    with open('/scratch/speech/outputs.pkl','wb+') as f:
+        pickle.dump(outputs,f)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output_directory', type=str,
+                        help='directory to save checkpoints')
+    parser.add_argument('-l', '--log_directory', type=str,
+                        help='directory to save tensorboard logs')
+    parser.add_argument('-c', '--checkpoint_path', type=str, default=None,
+                        required=False, help='checkpoint path')
+    parser.add_argument('--warm_start', action='store_true',
+                        help='load model weights only, ignore specified layers')
+    parser.add_argument('--n_gpus', type=int, default=1,
+                        required=False, help='number of gpus')
+    parser.add_argument('--rank', type=int, default=0,
+                        required=False, help='rank of current gpu')
+    parser.add_argument('--group_name', type=str, default='group_name',
+                        required=False, help='Distributed group name')
+    parser.add_argument('--hparams', type=str,
+                        required=False, help='comma separated name=value pairs')
+    parser.add_argument('--latent', type=int, default=1, required=False, help='0 if want to train tacotron')
+
+    args = parser.parse_args()
+    hparams = create_hparams(args.hparams)
+
     evaluate_latent_model('/scratch/speech/output/IEMOCAP/checkpoint_330000')
