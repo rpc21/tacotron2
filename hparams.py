0b26c37a74829d7a6e8f94e43b78d4596c096626
Ryan Culhane <rpc21@research-tarokhlab-03.oit.duke.edu>
2019-07-22 16:00:33 -0400

    hparams

--- hparams.py
+++ hparams.py
@@ -26,14 +26,14 @@ def create_hparams(hparams_string=None, verbose=False):
         ################################
         load_mel_from_disk=True, #False,
 #        training_files='filelists/ljs_audio_text_train_filelist.txt',
-        training_files='/scratch/speech/datasets/Tacotron_LibriTTS/LibriTTS_train_100.txt',
+#        training_files='/scratch/speech/datasets/Tacotron_LibriTTS/LibriTTS_train_100.txt',
 #        training_files='/scratch/speech/datasets/IEMOCAP_happy_sad_train.pkl',
-#        training_files='/scratch/speech/datasets/IEMOCAP_fru_train.pkl',
+        training_files='/scratch/speech/datasets/IEMOCAP_fru_train.pkl',
 #        training_files='/scratch/speech/datasets/Tacotron_LibriTTS/LibriTTS_train_100_1028_subset.txt',
 #        training_files='/scratch/speech/datasets/Tacotron_LibriTTS/LibriTTS_train_small.txt',
-        validation_files='/scratch/speech/datasets/Tacotron_LibriTTS/LibriTTS_validation_100.txt',
+#        validation_files='/scratch/speech/datasets/Tacotron_LibriTTS/LibriTTS_validation_100.txt',
 #        validation_files='/scratch/speech/datasets/IEMOCAP_happy_sad_val.pkl',
-#        validation_files='/scratch/speech/datasets/IEMOCAP_fru_val.pkl',
+        validation_files='/scratch/speech/datasets/IEMOCAP_fru_val.pkl',
 #        validation_files='/scratch/speech/datasets/Tacotron_LibriTTS/LibriTTS_validation_small.txt',
         text_cleaners=['english_cleaners'],
 
@@ -101,7 +101,7 @@ def create_hparams(hparams_string=None, verbose=False):
         learning_rate=1e-3,
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
-        batch_size=2,  #8,
+        batch_size=6,  #8,
         mask_padding=True  # set model's padded outputs to padded values
     )
 
